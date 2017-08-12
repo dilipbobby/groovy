7f1a967f294055dcc45e7278c427963740f115f3
dilipbobby <dilipbobby@users.noreply.github.com>
2017-08-13 00:23:49 +0530

    Update for.groovy

--- for.groovy
+++ for.groovy
@@ -1,9 +1,10 @@
-class Example { 
+/*author:Dilip bobby*/
+class For { 
    static void main(String[] args) {
 	
       for(int i = 0;i<5;i++) {
 	     println(" hello Jenkins " +i);
-      }
+      }//for close
 		
-   }
-}
+   }//main close
+}//class close
